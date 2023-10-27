3730e128ed68fe6ecf193cf05025dfadcfa12eb8
Leo Johnson <leochinonejohnson@gmail.com>
2023-10-27 17:00:18 -0500

    Added necessary html pieces, added button press functionality, added delete button press. Troubleshooting monthly salary counter lowering... jeez

--- script.js
+++ script.js
@@ -1,16 +1,79 @@
-function startUp () {
-    console.log('IT\S ALIIIIIIIIVE!')
-}
 startUp();
 
-function addEmployee() {
-    console.log('addEmployee called.')
+function startUp () {
+    console.log('IT\S ALIIIIIIIIVE!');
+}
+ 
+let totalSalary = 0;
+
+// submit button press
+function addEmployee(event) {
+
+    // immediate actions
+    event.preventDefault();
+    console.log('addEmployee called.');
+
+    // get input data
+    let formFirstName = document.getElementById('firstNameInput').value
+    let formLastName = document.getElementById('lastNameInput').value
+    let formIdNumber = document.getElementById('idNumberInput').value
+    let formJobTitle = document.getElementById('jobTitleInput').value
+    let formAnnualSalary = document.getElementById('annualSalaryInput').value
+
+    // store input in table
+    let table = document.getElementById('table');
+    table.innerHTML +=
+    `
+        <tr>
+            <td>${formFirstName}</td>
+            <td>${formLastName}</td>
+            <td>${formIdNumber}</td>
+            <td>${formJobTitle}</td>
+            <td>${formAnnualSalary}</td>
+            <td><button onclick="deleteEmployee(event)">❌</button></td>
+        </tr>
+    `
+
+    // update total salary
+    totalSalary += Number(formAnnualSalary)
+    document.getElementById('counter').innerHTML = totalSalary/12
+
+    // over-budget indicator
+    let footer = document.querySelector('footer');
+    if (totalSalary/12 > 20000) {
+        footer.classList.add('red-text');
+    }
+
+    // clear form inputs
+    document.getElementById('firstNameInput').value = '';
+    document.getElementById('lastNameInput').value = '';
+    document.getElementById('idNumberInput').value = '';
+    document.getElementById('jobTitleInput').value = '';
+    document.getElementById('annualSalaryInput').value = '';
 }
 
-function deleteEmployee () {
-    console.log('deleteEmployee called.')
+// delete button press
+function deleteEmployee (event) {
+    console.log('deleteEmployee called.');
+
+    // reflects in monthly total
+        // take the annual salary of the clicked tr
+        let whatIsThis = event.target.parentElement.parentElement.getElementById('remover');
+        console.log(whatIsThis)
+
+    // remove salary from total
+
+    // make red text go away if below 20k
+    if (totalSalary <= 20000) {
+        let footer = document.querySelector('footer');
+        footer.classList.remove('red-text');
+    }
+
+    // removes <tr>
+    event.target.parentElement.parentElement.remove();
 }
 
+// is this necessary?
 function overBudget () {
-    console.log('overBudget called.')
+    console.log('overBudget called.');
 }
