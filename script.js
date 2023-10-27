77cdf586921974c1daf8cf2cdb8bbef7759dd94e
Leo Johnson <leochinonejohnson@gmail.com>
2023-10-27 14:55:32 -0500

    Added some preliminary empty functions.

---
+++ script.js
@@ -0,0 +1,16 @@
+function startUp () {
+    console.log('IT\S ALIIIIIIIIVE!')
+}
+startUp();
+
+function addEmployee() {
+    console.log('addEmployee called.')
+}
+
+function deleteEmployee () {
+    console.log('deleteEmployee called.')
+}
+
+function overBudget () {
+    console.log('overBudget called.')
+}
